3973f43a6c59b2894ed2d64486cd2711d07f1545
Samrudhi Nayak <snayak6@uic.edu>
2016-10-31 16:14:15 -0500

    get first clone_url

--- build.sbt
+++ build.sbt
@@ -8,3 +8,6 @@ libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-http-experimental" % "2.0.1",
   "org.scalatest"   %% "scalatest"    % "2.2.4"   % "test"
 )
+
+// https://mvnrepository.com/artifact/com.typesafe.play/play-json_2.10
+libraryDependencies += "com.typesafe.play" % "play-json_2.11" % "2.4.0-M1"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,27 +1,60 @@
 /**
   * Created by samrudhinayak on 10/27/16.
   */
+
+import play.api.libs.json._
 import akka.actor.ActorSystem
-import akka.util.ByteString
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
 
 import scala.concurrent.{Await, Future}
 import akka.stream.ActorMaterializer
-import akka.http.scaladsl.model._
-import akka.http.scaladsl.Http
+import akka.util.ByteString
 import scala.concurrent.duration._
 
-object Main extends App {
+  object Main extends App {
 
-  implicit val system = ActorSystem()
-  implicit val materializer = ActorMaterializer()
+    implicit val system = ActorSystem()
+    implicit val materializer = ActorMaterializer()
 
-  val responseFuture: Future[HttpResponse] =
-    Http().singleRequest(HttpRequest(uri = "https://api.github.com/users/square/repos"))
+    val uri = "https://api.github.com/users/square/repos"
+          val urls = scala.io.Source.fromURL(uri).mkString
+        //println(urls)
+    val obj = Json.parse(urls)
+    println(obj)
+    var firstObj = obj(0);
+    var clone_url = firstObj.\("clone_url")
+    println(clone_url)
+//val responseFuture: Future[HttpResponse] =
+//Http().singleRequest(HttpRequest(uri = "http://akka.io"))
+//
+//    import system.dispatcher
+//
+//    val response = Await.result(responseFuture, 5.seconds)
+//
+//    response.entity.dataBytes.runFold(ByteString.empty)(_ ++ _).map(_.utf8String).foreach(println)
+//    println(response.getClass.getSimpleName)
+//
+//    val json = EntityUtils.
+  }
 
-  import system.dispatcher
 
-  val response = Await.result(responseFuture, 10.seconds)
+  //val ans= Await.result(p,5.seconds)
 
-  response.entity.dataBytes.runFold(ByteString.empty)(_ ++ _).map(_.utf8String).foreach(println)
-  
-}
+  //println("hello  " +p)
+
+  //println("type   " +p.getClass.getSimpleName)
+
+
+//      val uri = "https://api.github.com/users/square/repos"
+//      val urls = scala.io.Source.fromURL(uri).mkString
+//    //println(urls)
+//    val ur = urls.replace("[",""""")
+//    val u = ur.replace("]","")
+//      println(u)
+//     //Json.parse(u)
+//      val cloner = urls.split(",").toList
+//      println(cloner)
+//      val patt = "clone_url:"
+//    cloner.foreach(println)
+
