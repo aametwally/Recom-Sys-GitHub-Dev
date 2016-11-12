f2a90f172bf4a9df578d9ba1c3cbdbb218634efe
Ahmed Metwally <ametwa2@uic.edu>
2016-11-11 21:49:31 -0600

    add writing to file method

--- src/main/scala/AnalyzerActor.scala
+++ src/main/scala/AnalyzerActor.scala
@@ -8,6 +8,8 @@ import java.nio.file.Path
 import java.io._
 import java.util.function.Consumer
 import org.jgrapht.graph._
+import java.util.HashMap
+import java.util.Set
 
 import com.scitools.understand._
 
@@ -19,13 +21,17 @@ object AnalyzerActor {
     var versionnames: Array[String] = Array("","")
     var num_added_nodes = 0
     var num_deleted_nodes = 0
+    // probably permanently unused
     var num_same_nodes = 0
     var dg_objs = Array(new DefaultDirectedGraph[String, DefaultEdge](classOf[DefaultEdge]),
-      new DefaultDirectedGraph[String, DefaultEdge](classOf[DefaultEdge]))
+                        new DefaultDirectedGraph[String, DefaultEdge](classOf[DefaultEdge]))
     var dg_objs_sorted = Array(new DefaultDirectedGraph[String, DefaultEdge](classOf[DefaultEdge]),
-      new DefaultDirectedGraph[String, DefaultEdge](classOf[DefaultEdge]))
+                               new DefaultDirectedGraph[String, DefaultEdge](classOf[DefaultEdge]))
     var added_vertices_array = Array[String]()
     var deleted_vertices_array = Array[String]()
+    var called_functions_in_addedfns_list = new HashMap[String,Array[String]]()
+    var called_functions_in_deletedfns_list = new HashMap[String,Array[String]]()
+    var outgoing_verts = Array[String]()
 
     // create 2 udb files (one for each version)
     def createUDBFiles (repoString:String): Array[String] = {
@@ -114,13 +120,22 @@ object AnalyzerActor {
       val vertex_array0 = dg_objs_sorted(0).vertexSet().toArray()
       val vertex_array1 = dg_objs_sorted(1).vertexSet().toArray()
 
+      // parse dependency graphs finding added and deleted vertices
+      // ... as well as quantity of added and deleted vertices
       for (vert <- vertex_array0) {
         if (vertex_array1.contains(vert)) {
+          // probably permanently unused
           num_same_nodes = num_same_nodes + 1
         }
         else {
           num_deleted_nodes = num_deleted_nodes + 1
           deleted_vertices_array = deleted_vertices_array :+ vert.toString()
+          outgoing_verts = Array[String]()
+          dg_objs_sorted(0).outgoingEdgesOf(vert.toString()).forEach(new Consumer[DefaultEdge] {
+            def accept(s: DefaultEdge) =
+              outgoing_verts = outgoing_verts :+ dg_objs_sorted(0).getEdgeTarget(s)
+          })
+          called_functions_in_deletedfns_list.put(vert.toString(),outgoing_verts)
         }
       }
 
@@ -128,42 +143,97 @@ object AnalyzerActor {
         if (!vertex_array0.contains(vert)) {
           num_added_nodes = num_added_nodes + 1
           added_vertices_array = added_vertices_array :+ vert.toString()
+          outgoing_verts = Array[String]()
+          dg_objs_sorted(1).outgoingEdgesOf(vert.toString()).forEach(new Consumer[DefaultEdge] {
+            def accept(s: DefaultEdge) =
+              outgoing_verts = outgoing_verts :+ dg_objs_sorted(1).getEdgeTarget(s)
+          })
+          called_functions_in_addedfns_list.put(vert.toString(),outgoing_verts)
         }
       }
     }
 
-    def displayResults(): Unit =
+    def writeResultsToFile(repoString:String): Unit =
     {
-      println("Added functions which can be tested:")
-      for (i <- added_vertices_array) {
-        println("Added vertex: " + i)
-      }
-      println(" ")
-      println("Deleted functions different from previous version:")
-      for (i <- deleted_vertices_array) {
-        println("Deleted vertex: " + i)
+      // write all final results to text files
+      val fdir = repoString + "/Analysis";
+      
+      var newVersion=  Process(Seq("mkdir", fdir))!!; 
+      
+      // create/add to function subdirectories here
+      val adddir = repoString + "/Analysis/addedfns";
+      newVersion=  Process(Seq("mkdir", adddir))!!;
+      // create/add to function subdirectories here
+      val dldir = repoString + "/Analysis/deletedfns";
+      newVersion=  Process(Seq("mkdir", dldir))!!;
+
+      val temp_write_file = new PrintWriter(new File(repoString + "/Analysis" +"/primaryDependencyGraphDifferences.txt"))
+
+      temp_write_file.write("Number of functions added " + num_added_nodes.toString() + "\n")
+      temp_write_file.write("Number of deleted functions " + num_deleted_nodes.toString() + "\n")
+
+      temp_write_file.write("\nAdded functions which should be tested:")
+      if (added_vertices_array == 0)
+          temp_write_file.write("none\n")
+      for (i <- added_vertices_array)
+      {
+        temp_write_file.write("Added dependency graph vertex: " + i + "\n")
+
+        val add_temp_writer = new PrintWriter(new File("/" + repoString + "Analysis" +"/addedfns/" + i + ".txt"))
+        add_temp_writer.write("Number of called fns in this fn: " + called_functions_in_addedfns_list.get(i).size + "\n")
+        if (called_functions_in_addedfns_list.get(i).size == 0)
+            add_temp_writer.write("none\n")
+        for (j <- called_functions_in_addedfns_list.get(i))
+        {
+          add_temp_writer.write("function: " + j + "\n")
+        }
+        add_temp_writer.close()
       }
 
-      println("STATS:")
-      println("Number of functions added: " + num_added_nodes.toString())
-      println("Number of functions deleted: " + num_deleted_nodes.toString())
-      println(" ")
-      println(" ")
+      println("\nDeleted functions (in v1 that aren't in v2):\n")
+      if (deleted_vertices_array == 0)
+        temp_write_file.write("none\n")
+      for (i <- deleted_vertices_array)
+      {
+        temp_write_file.write("Deleted dependency graph vertex: " + i + "\n")
+
+        val del_temp_writer = new PrintWriter(new File("/" + repoString + "Analysis" +"/deletedfns/" + i + ".txt"))
+        del_temp_writer.write("Number of called fns in this fn: " + called_functions_in_deletedfns_list.size() + "\n")
+        if (called_functions_in_deletedfns_list.get(i).size == 0)
+            del_temp_writer.write("none\n")
+        for (j <- called_functions_in_deletedfns_list.get(i))
+        {
+          del_temp_writer.write("function: " + j + "\n")
+        }
+        del_temp_writer.close()
+      }
+
+      temp_write_file.close()
+
+      println("Done writing to all files...")
     }
 
-    def analyzeResults(): Unit =
+    def displayResults(repoString:String): Unit =
+    {
+      println("Please open " + repoString + "/" + "primaryDependencyGraphDifferences.txt to see the primary results for this project (subfolder in project root directory)")
+      println("Examine text files in " + repoString + "/deletedfns/ to see functions called in each deleted function for this project (subfolder in project root directory)")
+      println("Examine text files in " + repoString + "/addedfns/ to see functions called in each added function for this project (subfolder in project root directory)")
+    }
+
+    def analyzeResults(repoString:String): Unit =
     {
       // analysis part
       setupSortedArray()
       calculateResults()
-      displayResults()
+      writeResultsToFile(repoString)
+      displayResults(repoString)
     }
 
     def receive= {
       case repoString: String => {
         val list_of_version_subfolders = createUDBFiles(repoString)
         createDepGraphs(list_of_version_subfolders)
-        analyzeResults()
+        analyzeResults(repoString)
       }
     }
 
